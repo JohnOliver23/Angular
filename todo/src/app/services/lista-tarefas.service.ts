f0c346a4131ccf116627de42d67866ef064d7e1c
john <johnoliver021@gmail.com>
2021-06-29 16:55:04 -0300

    criando um contador com o numero de tarefas pendentes, criar botão para excluir todas as tarefas, permitir cadastrar uma nova tarefa ao pressionar a tecla enter e corrigir bug que permite cadastrar atividades em branco

--- todo/src/app/services/lista-tarefas.service.ts
+++ todo/src/app/services/lista-tarefas.service.ts
@@ -5,22 +5,36 @@ import { TarefaModel } from './tarefa-model';
 })
 export class ListaTarefasService {
   public lista: Array<TarefaModel> = [];
+  public contadorTarefasPendentes = 0;
   private proximoId = 1; // controle interno de ID
 
   constructor() {}
 
   public adicionar(descricao: string): void {
-    const nova = new TarefaModel();
-    nova.id = this.proximoId;
-    nova.descricao = descricao;
-    this.lista.push(nova);
-    this.proximoId++;
+    if (!descricao) {
+      alert('Informe uma tarefa válida');
+    } else {
+      const nova = new TarefaModel();
+      nova.id = this.proximoId;
+      nova.descricao = descricao;
+      this.lista.push(nova);
+      this.proximoId++;
+      this.contadorTarefasPendentes++;
+    }
+  }
+
+  public excluir(): void {
+    this.lista = [];
+    this.contadorTarefasPendentes = 0;
   }
 
   public atualizar(tarefaid: number): void {
     const tarefaEncontrada = this.lista.find((x) => x.id === tarefaid);
     if (tarefaEncontrada) {
       tarefaEncontrada.concluida = !tarefaEncontrada.concluida; // Atualizar status
+      tarefaEncontrada.concluida
+        ? this.contadorTarefasPendentes--
+        : this.contadorTarefasPendentes++;
     }
   }
 }
