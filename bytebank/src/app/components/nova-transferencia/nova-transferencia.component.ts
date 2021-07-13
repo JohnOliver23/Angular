5a0dc98473118b246f37a36713263ad4117f3829
john <johnoliver021@gmail.com>
2021-07-13 18:34:59 -0300

    Projeto bytebank: exportando dados com output e recebendo dados no componente

---
+++ bytebank/src/app/components/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent implements OnInit {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  public valor: number | null = null;
+  public destino: string = '';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+  }
+}
